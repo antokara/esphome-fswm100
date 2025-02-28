f8fc19c20ab4f3182b55829a2ae2252991c900bd
Antonios Karagiannis <antokarag@gmail.com>
2025-02-28 15:13:24 -0700

    add test publish

--- esphome/components/fswm100/fswm100.cpp
+++ esphome/components/fswm100/fswm100.cpp
@@ -6,12 +6,15 @@ namespace fswm100 {
 
 static const char *TAG = "fswm100";
 
-// void FSWM100::setup() {}
-
-void FSWM100::loop() {}
-
-// void FSWM100::update() { this->publish_state(1.2345); }
-
+void FSWM100::loop() {
+  const uint32_t now = millis();
+  if (now - this->last_transmission_ >= 1000) {
+    this->last_transmission_ = now;
+    this->pressure_sensor_->publish_state(1.23f);
+    this->flow_sensor_->publish_state(2.34f);
+    this->pulse_sensor_->publish_state(1);
+  }
+}
 void FSWM100::dump_config() { ESP_LOGCONFIG(TAG, "FSWM100..."); }
 
 }  // namespace fswm100
--- esphome/components/fswm100/fswm100.h
+++ esphome/components/fswm100/fswm100.h
@@ -8,16 +8,53 @@ namespace fswm100 {
 
 class FSWM100 : public Component {
  public:
-  void loop() override;
+  /**
+   * @brief Sets the flow sensor object.
+   * @see sensor.py:to_code(config)
+   *
+   * @param flow_sensor
+   */
   void set_flow_sensor(sensor::Sensor *flow_sensor) { flow_sensor_ = flow_sensor; }
+
+  /**
+   * @brief Set the pulse sensor object
+   * @see sensor.py:to_code(config)
+   *
+   * @param pulse_sensor
+   */
   void set_pulse_sensor(sensor::Sensor *pulse_sensor) { pulse_sensor_ = pulse_sensor; }
+
+  /**
+   * @brief Set the pressure sensor object
+   * @see sensor.py:to_code(config)
+   *
+   * @param pressure_sensor
+   */
   void set_pressure_sensor(sensor::Sensor *pressure_sensor) { pressure_sensor_ = pressure_sensor; }
+
+  void loop() override;
   void dump_config() override;
 
  protected:
+  /**
+   * @brief the flow sensor
+   *
+   */
   sensor::Sensor *flow_sensor_{nullptr};
+
+  /**
+   * @brief the pulse sensor
+   *
+   */
   sensor::Sensor *pulse_sensor_{nullptr};
+
+  /**
+   * @brief the pressure sensor
+   *
+   */
   sensor::Sensor *pressure_sensor_{nullptr};
+
+  uint32_t last_transmission_{0};
 };
 
 }  // namespace fswm100
