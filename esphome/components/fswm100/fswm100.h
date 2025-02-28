043fc5adbf882d472dc5ca2398b2b63bbe62a2b0
Antonios Karagiannis <antokarag@gmail.com>
2025-02-28 14:48:39 -0700

    add more sensors

--- esphome/components/fswm100/fswm100.h
+++ esphome/components/fswm100/fswm100.h
@@ -8,14 +8,16 @@ namespace fswm100 {
 
 class FSWM100 : public Component {
  public:
-  // void setup() override;
   void loop() override;
   void set_flow_sensor(sensor::Sensor *flow_sensor) { flow_sensor_ = flow_sensor; }
-  // void update() override;
+  void set_pulse_sensor(sensor::Sensor *pulse_sensor) { pulse_sensor_ = pulse_sensor; }
+  void set_pressure_sensor(sensor::Sensor *pressure_sensor) { pressure_sensor_ = pressure_sensor; }
   void dump_config() override;
 
  protected:
   sensor::Sensor *flow_sensor_{nullptr};
+  sensor::Sensor *pulse_sensor_{nullptr};
+  sensor::Sensor *pressure_sensor_{nullptr};
 };
 
 }  // namespace fswm100
--- esphome/components/fswm100/sensor.py
+++ esphome/components/fswm100/sensor.py
@@ -1,7 +1,18 @@
 import esphome.codegen as cg
 from esphome.components import sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_FLOW, CONF_ID, DEVICE_CLASS_VOLUME_FLOW_RATE, ICON_WATER
+from esphome.const import (
+    CONF_FLOW,
+    CONF_ID,
+    CONF_PRESSURE,
+    DEVICE_CLASS_EMPTY,
+    DEVICE_CLASS_PRESSURE,
+    DEVICE_CLASS_VOLUME_FLOW_RATE,
+    ICON_GAUGE,
+    ICON_PULSE,
+    ICON_WATER,
+    UNIT_EMPTY,
+)
 
 # @see https://github.com/elupus/home-assistant/blob/ffc5f436eedbbc4920fe16b809681d83cfddb3af/homeassistant/const.py#L1045
 GALLONS_PER_MINUTE = "gal/min"
@@ -30,18 +41,18 @@ CONFIG_SCHEMA = cv.Schema(
             accuracy_decimals=2,
             device_class=DEVICE_CLASS_VOLUME_FLOW_RATE,
         ),
-        # cv.Optional(CONF_PULSE): sensor.sensor_schema(
-        #     unit_of_measurement=UNIT_EMPTY,
-        #     icon=ICON_PULSE,
-        #     accuracy_decimals=0,
-        #     device_class=DEVICE_CLASS_EMPTY,
-        # ),
-        # cv.Optional(CONF_PRESSURE): sensor.sensor_schema(
-        #     unit_of_measurement=UNIT_PSI,
-        #     icon=ICON_GAUGE,
-        #     accuracy_decimals=2,
-        #     device_class=DEVICE_CLASS_PRESSURE,
-        # ),
+        cv.Optional(CONF_PULSE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_PULSE,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
+        ),
+        cv.Optional(CONF_PRESSURE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_PSI,
+            icon=ICON_GAUGE,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_PRESSURE,
+        ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -58,3 +69,9 @@ async def to_code(config):
     if flow_config := config.get(CONF_FLOW):
         sens = await sensor.new_sensor(flow_config)
         cg.add(var.set_flow_sensor(sens))
+    if pulse_config := config.get(CONF_PULSE):
+        sens = await sensor.new_sensor(pulse_config)
+        cg.add(var.set_pulse_sensor(sens))
+    if pressure_config := config.get(CONF_PRESSURE):
+        sens = await sensor.new_sensor(pressure_config)
+        cg.add(var.set_pressure_sensor(sens))
