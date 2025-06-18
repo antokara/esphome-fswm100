203d33de5df4838025f8899cdcf9c4ad92e987e2
Antonios Karagiannis <antokarag@gmail.com>
2025-06-17 19:34:15 -0600

    change pressure logging

--- esphome/components/fswm100/pressure_sensor.cpp
+++ esphome/components/fswm100/pressure_sensor.cpp
@@ -70,8 +70,10 @@ void PressureSensor::loop() {
   if (abs(this->last_sensor_state_ - new_sensor_state) > this->effective_noise_floor_) {
     float pressure =
         this->voltage_to_pressure(new_sensor_state) * this->fswm100_->get_pressure_sensor_calibration_multiplier();
-    ESP_LOGV(TAG, "'%s': Voltage Delta %.4f", this->get_name().c_str(),
-             abs(this->last_sensor_state_ - new_sensor_state));
+
+    ESP_LOGV(TAG, "'%s': Voltage %.4f", new_sensor_state, pressure);
+    ESP_LOGVV(TAG, "'%s': Voltage Delta %.4f", this->get_name().c_str(),
+              abs(this->last_sensor_state_ - new_sensor_state));
     ESP_LOGV(TAG, "'%s': Converted to %.4f pressure", this->get_name().c_str(), pressure);
 
     if (pressure < this->min_pressure_) {
