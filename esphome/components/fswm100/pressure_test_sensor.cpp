4e2fbd4e56c3c365f23822874f8aed15c2233380
Antonios Karagiannis <antokarag@gmail.com>
2025-07-04 14:16:05 -0600

    change log lvl

--- esphome/components/fswm100/pressure_test_sensor.cpp
+++ esphome/components/fswm100/pressure_test_sensor.cpp
@@ -43,7 +43,7 @@ void PressureTestSensor::process(float pressure) {
 
 void PressureTestSensor::reset_filters() {
   if (this->filters_factory_) {
-    ESP_LOGI(TAG, "PressureTestSensor Resetting filters by creating new filter instances...");
+    ESP_LOGD(TAG, "PressureTestSensor Resetting filters by creating new filter instances...");
     this->set_filters(this->filters_factory_());
   } else {
     ESP_LOGW(TAG, "PressureTestSensor No filter factory set, cannot reset filters.");
