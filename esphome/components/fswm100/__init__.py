ef3ea33cbad5de8659b8276755ee5cf8036842f1
Antonios Karagiannis <antokarag@gmail.com>
2025-07-05 14:35:19 -0600

    add throttle_average support

--- esphome/components/fswm100/__init__.py
+++ esphome/components/fswm100/__init__.py
@@ -443,6 +443,11 @@ async def build_filters(config):
                 # convert the "absolute" or "percentage" string to a percentage boolean
                 # meaning, pass true for "percentage" and false for "absolute"
                 formatted_args.append("true" if arg == "percentage" else "false")
+            elif filter_key == "throttle_average":
+                # convert string to milliseconds
+                formatted_args.append(
+                    str(cv.time_period_str_unit(arg).total_milliseconds)
+                )
             elif isinstance(arg, str) and not isinstance(
                 arg, (cv.Lambda, cg.RawExpression)
             ):
