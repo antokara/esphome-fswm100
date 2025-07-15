5b9b1d18f5adea02361eb6b9005a2bea9d9878ee
Antonios Karagiannis <antokarag@gmail.com>
2025-07-14 18:48:46 -0600

    test flow calc

--- esphome/components/fswm100/flow_sensor.cpp
+++ esphome/components/fswm100/flow_sensor.cpp
@@ -51,11 +51,13 @@ float FlowSensor::calculate_active_flow() {
     const uint32_t time_between_pulses =
         abs(long(this->newest_pulse_sensor_active_time_ - this->oldest_pulse_sensor_active_time_));
     const uint32_t time_since_newest_pulse = abs(long(millis() - this->newest_pulse_sensor_active_time_));
-    if (time_since_newest_pulse > time_between_pulses) {
-      // if the time since the last pulse is greater than the time between pulses,
-      // we can calculate the new flow rate
-      flow = (this->rate_time_ * 1000) / (millis() - this->oldest_pulse_sensor_active_time_) *
-             this->fswm100_->get_pulse_rate_volume();
+    const float calculated_flow = (this->rate_time_ * 1000) / (millis() - this->oldest_pulse_sensor_active_time_) *
+                                  this->fswm100_->get_pulse_rate_volume();
+    if (calculated_flow > this->state || time_since_newest_pulse > time_between_pulses) {
+      // if the the current flow is greater than the last published one or
+      // if time since the last pulse is greater than the time between pulses,
+      // we use the newly calculated flow rate.
+      flow = calculated_flow;
     } else if (this->state > 0) {
       // when not enough time has passed to calculate the new flow rate but there is an active flow,
       // use the last published flow rate
